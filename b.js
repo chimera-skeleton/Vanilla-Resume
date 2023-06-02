7cd98aa37d8b8149e0aa82e576b27e6ab5fa9941
Scoheart <Scoheart1487790559@gmail.com>
2023-06-02 19:41:27 +0800

    feat: add babel to transform jsx to html

---
+++ b.js
@@ -0,0 +1,100 @@
+SReact.createElement("div", {
+  class: "content"
+}, SReact.createElement("div", {
+  class: "title-wrapper"
+}, SReact.createElement("div", {
+  class: "left-wrapper"
+}, SReact.createElement("h1", null, "Scoheart"), SReact.createElement("p", {
+  class: "career"
+}, "Full-Stack Learner"), SReact.createElement("p", {
+  class: "intro"
+}, "Hi, here! I'm Scoheart, a student studying at the JS University. In my free time, I like to explore unknown domain, such as React on the Front-End, Spring on the Back-End.")), SReact.createElement("div", {
+  class: "right-wrapper"
+}, SReact.createElement("img", {
+  src: "image/scoheart.jpg",
+  alt: ""
+}))), SReact.createElement("div", {
+  class: "subtitle"
+}, SReact.createElement("h2", null, "Running in the Dark"), SReact.createElement("p", null, "I'm building web apps using native web components on my job. But, I love exploring new build tools, programing languages and frameworks. Here is a list of technologies that I have my eyes on.")), SReact.createElement("div", {
+  class: "tech-stack"
+}, SReact.createElement("div", {
+  class: "tech1"
+}, SReact.createElement("div", {
+  class: "icon-wrapper"
+}, SReact.createElement("span", {
+  class: "icon"
+}, SReact.createElement("img", {
+  src: "./image/javascript-color.svg",
+  alt: ""
+})), SReact.createElement("span", {
+  class: "tech-name"
+}, "JavaScript")), SReact.createElement("div", {
+  class: "icon-wrapper"
+}, SReact.createElement("span", {
+  class: "icon"
+}, SReact.createElement("img", {
+  src: "./image/react-color.svg",
+  alt: ""
+})), SReact.createElement("span", {
+  class: "tech-name"
+}, "React"))), SReact.createElement("div", {
+  class: "tech1"
+}, SReact.createElement("div", {
+  class: "icon-wrapper"
+}, SReact.createElement("span", {
+  class: "icon"
+}, SReact.createElement("img", {
+  src: "./image/vuedotjs-color.svg",
+  alt: ""
+})), SReact.createElement("span", {
+  class: "tech-name"
+}, "Vue.js")), SReact.createElement("div", {
+  class: "icon-wrapper"
+}, SReact.createElement("span", {
+  class: "icon"
+}, SReact.createElement("img", {
+  src: "./image/astro-color.svg",
+  alt: ""
+})), SReact.createElement("span", {
+  class: "tech-name"
+}, "Astro"))), SReact.createElement("div", {
+  class: "tech1"
+}, SReact.createElement("div", {
+  class: "icon-wrapper"
+}, SReact.createElement("span", {
+  class: "icon"
+}, SReact.createElement("img", {
+  src: "./image/nextdotjs-color.svg",
+  alt: ""
+})), SReact.createElement("span", {
+  class: "tech-name"
+}, "Next.js")), SReact.createElement("div", {
+  class: "icon-wrapper"
+}, SReact.createElement("span", {
+  class: "icon"
+}, SReact.createElement("img", {
+  src: "./image/svelte-color.svg",
+  alt: ""
+})), SReact.createElement("span", {
+  class: "tech-name"
+}, "Svelte"))), SReact.createElement("div", {
+  class: "tech1"
+}, SReact.createElement("div", {
+  class: "icon-wrapper"
+}, SReact.createElement("span", {
+  class: "icon"
+}, SReact.createElement("img", {
+  src: "./image/nodedotjs-color.svg",
+  alt: ""
+})), SReact.createElement("span", {
+  class: "tech-name"
+}, "Nodejs")), SReact.createElement("div", {
+  class: "icon-wrapper"
+}, SReact.createElement("span", {
+  class: "icon"
+}, SReact.createElement("img", {
+  src: "./image/typescript-color.svg",
+  alt: ""
+})), SReact.createElement("span", {
+  class: "tech-name"
+}, "TypeScript")))));
---
+++ router/content.js
@@ -0,0 +1,89 @@
+
+    <div class="content">
+        <div class="title-wrapper">
+            <div class="left-wrapper">
+                <h1>Scoheart</h1>
+                <p class="career">Full-Stack Learner</p>
+                <p class="intro">Hi, here! I'm Scoheart, a student studying at the JS University. In my free time, I
+                    like to
+                    explore unknown domain, such as React on the Front-End, Spring on the Back-End.</p>
+            </div>
+            <div class="right-wrapper">
+                <img src="image/scoheart.jpg" alt=""/>
+            </div>
+        </div>
+        <div class="subtitle">
+            <h2>Running in the Dark</h2>
+            <p>I'm building web apps using native web components on my job. But, I love exploring new build tools,
+                programing languages and frameworks. Here is a list of technologies that I have my eyes on.</p>
+        </div>
+        <div class="tech-stack">
+            <div class="tech1">
+                <div class="icon-wrapper">
+                    <span class="icon">
+                        <img src="./image/javascript-color.svg" alt=""/>
+                    </span>
+                    <span class="tech-name">JavaScript</span>
+                    
+                </div>
+                <div class="icon-wrapper">
+                    <span class="icon">
+                        <img src="./image/react-color.svg" alt=""/>
+                    </span>
+                    <span class="tech-name">React</span>
+
+                </div>
+            </div>
+
+            <div class="tech1">
+                <div class="icon-wrapper">
+                    <span class="icon">
+                        <img src="./image/vuedotjs-color.svg" alt=""/>
+                    </span>
+                    <span class="tech-name">Vue.js</span>
+
+                </div>
+                <div class="icon-wrapper">
+                    <span class="icon">
+                        <img src="./image/astro-color.svg" alt=""/>
+                    </span>
+                    <span class="tech-name">Astro</span>
+
+                </div>
+            </div>
+
+            <div class="tech1">
+                <div class="icon-wrapper">
+                    <span class="icon">
+                        <img src="./image/nextdotjs-color.svg" alt=""/>
+                    </span>
+                    <span class="tech-name">Next.js</span>
+
+                </div>
+                <div class="icon-wrapper">
+                    <span class="icon">
+                        <img src="./image/svelte-color.svg" alt=""/>
+                    </span>
+                    <span class="tech-name">Svelte</span>
+
+                </div>
+            </div>
+
+            <div class="tech1">
+                <div class="icon-wrapper">
+                    <span class="icon">
+                        <img src="./image/nodedotjs-color.svg" alt=""/>
+                    </span>
+                    <span class="tech-name">Nodejs</span>
+                </div>
+                <div class="icon-wrapper">
+                    <span class="icon">
+                        <img src="./image/typescript-color.svg" alt=""/>
+                    </span>
+                    <span class="tech-name">TypeScript</span>
+
+                </div>
+            </div>
+
+        </div>
+    </div>
